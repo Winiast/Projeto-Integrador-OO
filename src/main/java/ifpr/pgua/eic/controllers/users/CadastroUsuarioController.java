4bc2a0349c6bbd2a424740f89a59ddc1970009ff
Driinho <pedro.nascimento.henrique2003@gmail.com>
2023-01-26 20:38:20 -0300

    [feat]: Implementado o metodo cadastrar usuario, com a validação por email.

--- src/main/java/ifpr/pgua/eic/controllers/users/CadastroUsuarioController.java
+++ src/main/java/ifpr/pgua/eic/controllers/users/CadastroUsuarioController.java
@@ -2,10 +2,19 @@ package ifpr.pgua.eic.controllers.users;
 
 import java.util.Properties;
 
-import com.google.protobuf.Message;
-import com.mysql.cj.Session;
+import jakarta.mail.Address;
+import jakarta.mail.Authenticator;
+import jakarta.mail.Message;
+import jakarta.mail.MessagingException;
+import jakarta.mail.PasswordAuthentication;
+import jakarta.mail.Session;
+import jakarta.mail.Transport;
+import jakarta.mail.internet.AddressException;
+import jakarta.mail.internet.InternetAddress;
+import jakarta.mail.internet.MimeMessage;
 
 import io.github.palexdev.materialfx.controls.MFXTextField;
+import jakarta.mail.internet.MimeMessage;
 import javafx.fxml.FXML;
 
 public class CadastroUsuarioController {
@@ -20,6 +29,68 @@ public class CadastroUsuarioController {
     private MFXTextField tfSobrenome;
 
     public CadastroUsuarioController() {
+    }
+
+    @FXML
+    private void cadastrar() {
+
+        String email = tfEmail.getText();
+        String nome = tfNome.getText();
+        String sobrenome = tfSobrenome.getText();
+
+        Thread thread = new Thread(() -> {
+            enviarEmail(email);
+        });
+
+        thread.start();
+    }
+
+    private boolean enviarEmail(String email) {
+        try {
+
+            String msg = "Cadastro quase concluído, para finalizar o cadastro, insira está senha: GUSTA_VIADINHO na primeira vez em que for efetuar o login.";
+            String assunto = "Cadastro IFPR Emprestimos";
+
+            String remetente = "ifpr.emprestimos@gmail.com";
+            Message message = new MimeMessage(criarSessionMail());
+            message.setFrom(new InternetAddress(remetente)); // Remetente
+
+            Address[] toUser = InternetAddress // Destinatário(s)
+                    .parse(email.trim().toLowerCase());
+
+            message.setRecipients(Message.RecipientType.TO, toUser);
+            message.setSubject(assunto);// Assunto
+            message.setContent(msg, "text/html");
+            /** Método para enviar a mensagem criada */
+            Transport.send(message);
+
+            System.err.println("Feito!!!");
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+    private Session criarSessionMail() {
+        Properties props = new Properties();
+
+        props.put("mail.smtp.host", "smtp.gmail.com");
+        props.put("mail.smtp.socketFactory.port", 465);
+        props.put("mail.smtp.socketFactory.class", "javax.net.ssl.SSLSocketFactory");
+        props.put("mail.smtp.auth", true);
+        props.put("mail.smtp.port", 465);
+
+        Session session = Session.getDefaultInstance(props, new Authenticator() {
+
+            protected PasswordAuthentication getPasswordAuthentication() {
+                return new PasswordAuthentication("ifpr.emprestimos@gmail.com", "shmbgbhpwmcfnquf");
+            }
+        });
+
+        session.setDebug(true);
+
+        return session;
     }
 
 }
