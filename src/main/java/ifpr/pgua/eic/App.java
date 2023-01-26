ff5160a4cc0307f794ac9f5122821e28f3a4e32c
Driinho <pedro.nascimento.henrique2003@gmail.com>
2023-01-26 15:36:05 -0300

    [feat]: Criada a tela de Cadastro de usuário

--- src/main/java/ifpr/pgua/eic/App.java
+++ src/main/java/ifpr/pgua/eic/App.java
@@ -2,32 +2,35 @@ package ifpr.pgua.eic;
 
 import ifpr.pgua.eic.controllers.EsporteController;
 import ifpr.pgua.eic.controllers.auth.*;
+import ifpr.pgua.eic.controllers.users.CadastroUsuarioController;
 import ifpr.pgua.eic.utils.Navigator.BaseAppNavigator;
 import ifpr.pgua.eic.utils.Navigator.ScreenRegistryFXML;
 
 public class App extends BaseAppNavigator {
 
-        @Override
-        public String getHome() {
-                return "LOGIN";
-        }
+    @Override
+    public String getHome() {
+        return "CADASTRO_USUARIO";
+    }
 
-        @Override
-        public String getAppTitle() {
-                return "IFPR EMPRÉSTIMOS";
-        }
+    @Override
+    public String getAppTitle() {
+        return "IFPR EMPRÉSTIMOS";
+    }
 
-        @Override
-        public void registrarTelas() {
-                // TODO Auto-generated method stub
-                registraTela("CADASTRO", new ScreenRegistryFXML(getClass(), "fxml/sports/CadastroEsporte.fxml",
-                                (o) -> new EsporteController()));
+    @Override
+    public void registrarTelas() {
+        registraTela("CADASTRO_ESPORTE", new ScreenRegistryFXML(getClass(), "fxml/sports/CadastroEsporte.fxml",
+                (o) -> new EsporteController()));
 
-                registraTela("LOGIN",
-                                new ScreenRegistryFXML(getClass(), "fxml/auth/Login.fxml",
-                                                (o) -> new LoginController()));
-                registraTela("ALTERAR_SENHA",
-                                new ScreenRegistryFXML(getClass(), "fxml/auth/AlterarSenha.fxml",
-                                                (o) -> new AlterarSenhaController()));
-        }
+        registraTela("LOGIN", new ScreenRegistryFXML(getClass(), "fxml/auth/Login.fxml",
+                (o) -> new LoginController()));
+
+        registraTela("ALTERAR_SENHA", new ScreenRegistryFXML(getClass(), "fxml/auth/AlterarSenha.fxml",
+                (o) -> new AlterarSenhaController()));
+
+        registraTela("CADASTRO_USUARIO", new ScreenRegistryFXML(getClass(), "fxml/users/CadastroUsuario.fxml",
+                (o) -> new CadastroUsuarioController()));
+
+    }
 }
---
+++ src/main/java/ifpr/pgua/eic/controllers/users/CadastroUsuarioController.java
@@ -0,0 +1,20 @@
+package ifpr.pgua.eic.controllers.users;
+
+import io.github.palexdev.materialfx.controls.MFXTextField;
+import javafx.fxml.FXML;
+
+public class CadastroUsuarioController {
+
+    @FXML
+    private MFXTextField tfEmail;
+
+    @FXML
+    private MFXTextField tfNome;
+
+    @FXML
+    private MFXTextField tfSobrenome;
+
+    public CadastroUsuarioController() {
+    }
+
+}
