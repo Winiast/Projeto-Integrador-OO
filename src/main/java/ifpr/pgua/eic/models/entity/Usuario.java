9b8e2eed502b66a4bc322a557ca514c370c57deb
Driinho <pedro.nascimento.henrique2003@gmail.com>
2023-01-27 20:47:17 -0300

    [feat]: Criada a entidade Usuario

---
+++ src/main/java/ifpr/pgua/eic/models/entity/Usuario.java
@@ -0,0 +1,96 @@
+package ifpr.pgua.eic.models.entity;
+
+import java.time.LocalDateTime;
+
+public class Usuario {
+
+    private long id;
+    private String nome;
+    private String sobrenome;
+    private String email;
+    private String senha;
+    private LocalDateTime criadoEm;
+    private LocalDateTime atualizadoEm;
+    private boolean status;
+
+    public Usuario(long id, String nome, String sobrenome, String email, String senha, long criadoEm, long atualizadoEm,
+            boolean status) {
+        this.id = id;
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public Usuario(String nome, String sobrenome, String email, String senha) {
+        this.nome = nome;
+        this.sobrenome = sobrenome;
+        this.email = email;
+        this.senha = senha;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getSobrenome() {
+        return sobrenome;
+    }
+
+    public void setSobrenome(String sobrenome) {
+        this.sobrenome = sobrenome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public LocalDateTime getCriadoEm() {
+        return criadoEm;
+    }
+
+    public void setCriadoEm(LocalDateTime criadoEm) {
+        this.criadoEm = criadoEm;
+    }
+
+    public LocalDateTime getAtualizadoEm() {
+        return atualizadoEm;
+    }
+
+    public void setAtualizadoEm(LocalDateTime atualizadoEm) {
+        this.atualizadoEm = atualizadoEm;
+    }
+
+    public boolean isStatus() {
+        return status;
+    }
+
+    public void setStatus(boolean status) {
+        this.status = status;
+    }
+
+}
