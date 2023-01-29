c6e11870164836542d58e2d16a3d96f1240fe8a7
Driinho <pedro.nascimento.henrique2003@gmail.com>
2023-01-28 23:41:20 -0300

    [feat]: Criado arquivo FabricaConexoes para controlar as conexoes com o banco de dados

---
+++ src/main/java/ifpr/pgua/eic/models/FabricaConexoes.java
@@ -0,0 +1,48 @@
+package ifpr.pgua.eic.models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import ifpr.pgua.eic.utils.Env;
+
+public class FabricaConexoes {
+    private static final int MAX_CONEXOES = 5;
+
+    private static FabricaConexoes instance;
+
+    private Connection[] conexoes;
+
+    protected FabricaConexoes() {
+        conexoes = new Connection[MAX_CONEXOES];
+    }
+
+    public static FabricaConexoes getInstance() {
+        if (instance == null) {
+            instance = new FabricaConexoes();
+        }
+        return instance;
+    }
+
+    public Connection getConnection() throws SQLException {
+
+        String user = Env.get("DB_USER");
+        String password = Env.get("DB_PASSWORD");
+        String url = Env.get("DB_URL");
+
+        for (int i = 0; i < conexoes.length; i++) {
+            if (conexoes[i] == null || conexoes[i].isClosed()) {
+                try {
+                    Class.forName("com.mysql.cj.jdbc.Driver");
+                    conexoes[i] = DriverManager.getConnection(url,
+                            user,
+                            password);
+                    return conexoes[i];
+                } catch (ClassNotFoundException e) {
+                    throw new SQLException(e.getMessage());
+                }
+            }
+        }
+        throw new SQLException("Não há conexões disponíveis! Esqueceu de fechar?");
+    }
+}
