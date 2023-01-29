062b82791bcbf3ae2c230f7b65e02cf4b550147e
Driinho <pedro.nascimento.henrique2003@gmail.com>
2023-01-28 23:30:05 -0300

    [feat]: Criada a classe UsuarioRepository

---
+++ src/main/java/ifpr/pgua/eic/models/repositories/UsuarioRepository.java
@@ -0,0 +1,29 @@
+package ifpr.pgua.eic.models.repositories;
+
+import ifpr.pgua.eic.models.daos.UsuarioDao;
+import ifpr.pgua.eic.models.entity.Usuario;
+
+public class UsuarioRepository {
+
+    private UsuarioDao usuarioDao;
+
+    public UsuarioRepository(UsuarioDao usuarioDao) {
+        this.usuarioDao = usuarioDao;
+    }
+
+    public boolean cadastrar(Usuario usuario) {
+        return usuarioDao.cadastrar(usuario);
+    }
+
+    public boolean atualizar(Usuario usuario) {
+        return usuarioDao.atualizar(usuario);
+    }
+
+    public boolean excluir(Usuario usuario) {
+        return usuarioDao.excluir(usuario);
+    }
+
+    public Usuario buscarPorNome(String nome) {
+        return usuarioDao.buscarPorNome(nome);
+    }
+}
