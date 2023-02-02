879fe1fc39f4b31c924ffeb4a9a3d317a18d3384
Gustavo <gustavo.winiast@gmail.com>
2023-02-02 15:27:47 -0300

    [feat]: Integração do cadastro de equipamentos através da tela fxml

--- src/main/java/ifpr/pgua/eic/App.java
+++ src/main/java/ifpr/pgua/eic/App.java
@@ -4,9 +4,14 @@ import ifpr.pgua.eic.controllers.auth.*;
 import ifpr.pgua.eic.controllers.users.CadastroUsuarioController;
 import ifpr.pgua.eic.controllers.users.ListaUsuarioController;
 import ifpr.pgua.eic.models.FabricaConexoes;
+import ifpr.pgua.eic.models.daos.EquipamentoDao;
+import ifpr.pgua.eic.models.daos.EsporteDao;
 import ifpr.pgua.eic.models.daos.UsuarioDao;
 import ifpr.pgua.eic.models.entity.Usuario;
+import ifpr.pgua.eic.models.jdbc.EquipamentoJDBC;
+import ifpr.pgua.eic.models.jdbc.EsporteJDBC;
 import ifpr.pgua.eic.models.jdbc.UsuarioJDBC;
+import ifpr.pgua.eic.models.repositories.EquipamentoRepository;
 import ifpr.pgua.eic.models.repositories.UsuarioRepository;
 import ifpr.pgua.eic.controllers.equips.CadastroEquipamentosController;
 import ifpr.pgua.eic.controllers.equips.ListaEquipamentosController;
@@ -19,64 +24,72 @@ import ifpr.pgua.eic.utils.Navigator.ScreenRegistryFXML;
 
 public class App extends BaseAppNavigator {
 
-    public static Usuario usuarioLogado;
-    private FabricaConexoes fabricaConexoes = FabricaConexoes.getInstance();
-    private UsuarioRepository usuarioRepository;
-    private UsuarioDao usuarioDao;
+        public static Usuario usuarioLogado;
+        private FabricaConexoes fabricaConexoes = FabricaConexoes.getInstance();
+        private UsuarioRepository usuarioRepository;
+        private UsuarioDao usuarioDao;
 
-    @Override
-    public void init() throws Exception {
-        usuarioDao = new UsuarioJDBC(fabricaConexoes);
-        usuarioRepository = new UsuarioRepository(usuarioDao);
-    }
+        private EquipamentoRepository equipamentoRepository;
+        private EquipamentoDao equipamentoDao;
+        private EsporteDao esporteDao;
 
-    @Override
-    public String getHome() {
-        return "LOGIN";
-    }
+        @Override
+        public void init() throws Exception {
+                usuarioDao = new UsuarioJDBC(fabricaConexoes);
+                usuarioRepository = new UsuarioRepository(usuarioDao);
+                equipamentoDao = new EquipamentoJDBC(fabricaConexoes, esporteDao);
+                equipamentoRepository = new EquipamentoRepository(equipamentoDao);
+                esporteDao = new EsporteJDBC(fabricaConexoes);
 
-    @Override
-    public String getAppTitle() {
-        return "IFPR EMPRÉSTIMOS";
-    }
+        }
 
-    @Override
-    public void registrarTelas() {
+        @Override
+        public String getHome() {
+                return "CADASTRO_EQUIPAMENTO";
+        }
 
-        registraTela("CADASTRO_EMPRESTIMO",
-                new ScreenRegistryFXML(getClass(), "fxml/loan/CadastroEmprestimo.fxml",
-                        (o) -> new CadastroEmprestimoController()));
-        registraTela("LISTA_EMPRESTIMO",
-                new ScreenRegistryFXML(getClass(), "fxml/loan/ListaEmprestimo.fxml",
-                        (o) -> new ListaEmprestimoController()));
+        @Override
+        public String getAppTitle() {
+                return "IFPR EMPRÉSTIMOS";
+        }
 
-        registraTela("CADASTRO_ESPORTE", new ScreenRegistryFXML(getClass(), "fxml/sports/CadastroEsporte.fxml",
-                (o) -> new EsporteCadastroController()));
+        @Override
+        public void registrarTelas() {
 
-        registraTela("LISTA_ESPORTE", new ScreenRegistryFXML(getClass(), "fxml/sports/ListaEsporte.fxml",
-                (o) -> new EsporteListaController()));
+                registraTela("CADASTRO_EMPRESTIMO",
+                                new ScreenRegistryFXML(getClass(), "fxml/loan/CadastroEmprestimo.fxml",
+                                                (o) -> new CadastroEmprestimoController()));
+                registraTela("LISTA_EMPRESTIMO",
+                                new ScreenRegistryFXML(getClass(), "fxml/loan/ListaEmprestimo.fxml",
+                                                (o) -> new ListaEmprestimoController()));
 
-        registraTela("CADASTRO_USUARIO",
-                new ScreenRegistryFXML(getClass(), "fxml/users/CadastroUsuario.fxml",
-                        (o) -> new CadastroUsuarioController(usuarioRepository)));
-        registraTela("LISTA_USUARIO",
-                new ScreenRegistryFXML(getClass(), "fxml/users/ListaUsuario.fxml",
-                        (o) -> new ListaUsuarioController()));
-        registraTela("LOGIN",
-                new ScreenRegistryFXML(getClass(), "fxml/auth/Login.fxml",
-                        (o) -> new LoginController(usuarioRepository)));
+                registraTela("CADASTRO_ESPORTE", new ScreenRegistryFXML(getClass(), "fxml/sports/CadastroEsporte.fxml",
+                                (o) -> new EsporteCadastroController()));
 
-        registraTela("ALTERAR_SENHA",
-                new ScreenRegistryFXML(getClass(), "fxml/auth/AlterarSenha.fxml",
-                        (o) -> new AlterarSenhaController()));
+                registraTela("LISTA_ESPORTE", new ScreenRegistryFXML(getClass(), "fxml/sports/ListaEsporte.fxml",
+                                (o) -> new EsporteListaController()));
 
-        registraTela("CADASTRO_EQUIPAMENTO",
-                new ScreenRegistryFXML(getClass(), "fxml/equips/CadastroEquipamentos.fxml",
-                        (o) -> new CadastroEquipamentosController()));
+                registraTela("CADASTRO_USUARIO",
+                                new ScreenRegistryFXML(getClass(), "fxml/users/CadastroUsuario.fxml",
+                                                (o) -> new CadastroUsuarioController(usuarioRepository)));
+                registraTela("LISTA_USUARIO",
+                                new ScreenRegistryFXML(getClass(), "fxml/users/ListaUsuario.fxml",
+                                                (o) -> new ListaUsuarioController()));
+                registraTela("LOGIN",
+                                new ScreenRegistryFXML(getClass(), "fxml/auth/Login.fxml",
+                                                (o) -> new LoginController(usuarioRepository)));
 
-        registraTela("LISTA_EQUIPAMENTO",
-                new ScreenRegistryFXML(getClass(), "fxml/equips/ListaEquipamentos.fxml",
-                        (o) -> new ListaEquipamentosController()));
+                registraTela("ALTERAR_SENHA",
+                                new ScreenRegistryFXML(getClass(), "fxml/auth/AlterarSenha.fxml",
+                                                (o) -> new AlterarSenhaController()));
 
-    }
+                registraTela("CADASTRO_EQUIPAMENTO",
+                                new ScreenRegistryFXML(getClass(), "fxml/equips/CadastroEquipamentos.fxml",
+                                                (o) -> new CadastroEquipamentosController(equipamentoRepository)));
+
+                registraTela("LISTA_EQUIPAMENTO",
+                                new ScreenRegistryFXML(getClass(), "fxml/equips/ListaEquipamentos.fxml",
+                                                (o) -> new ListaEquipamentosController()));
+
+        }
 }
--- src/main/java/ifpr/pgua/eic/controllers/equips/CadastroEquipamentosController.java
+++ src/main/java/ifpr/pgua/eic/controllers/equips/CadastroEquipamentosController.java
@@ -1,21 +1,41 @@
 package ifpr.pgua.eic.controllers.equips;
 
+import java.net.URL;
+import java.util.ResourceBundle;
 import ifpr.pgua.eic.App;
+import ifpr.pgua.eic.models.FabricaConexoes;
+import ifpr.pgua.eic.models.daos.EsporteDao;
+import ifpr.pgua.eic.models.entity.Esporte;
+import ifpr.pgua.eic.models.entity.Estado;
+import ifpr.pgua.eic.models.jdbc.EsporteJDBC;
+import ifpr.pgua.eic.models.repositories.EquipamentoRepository;
+import ifpr.pgua.eic.models.repositories.EsporteRepository;
+import io.github.palexdev.materialfx.controls.MFXComboBox;
 import io.github.palexdev.materialfx.controls.MFXTextField;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 
-public class CadastroEquipamentosController {
+public class CadastroEquipamentosController implements Initializable {
+
+    private EquipamentoRepository equipamentoRepository;
+    private EsporteRepository esporteRepository;
+    private EsporteDao esporteDao;
+    private FabricaConexoes fabricaConexoes = FabricaConexoes.getInstance();
 
     @FXML
     private MFXTextField tfNomeEquipamento;
 
     @FXML
-    private MFXTextField tfDescricao;
-
-    @FXML
     private MFXTextField tfQuantidade;
 
-    public CadastroEquipamentosController() {
+    @FXML
+    private MFXComboBox<Estado> estadoInput;
+
+    @FXML
+    private MFXComboBox<Esporte> esporteInput;
+
+    public CadastroEquipamentosController(EquipamentoRepository equipamentoRepository) {
+        this.equipamentoRepository = equipamentoRepository;
     }
 
     @FXML
@@ -45,17 +65,33 @@ public class CadastroEquipamentosController {
 
     @FXML
     public void cadastrar() {
-        System.out.println("Cadastrar Equipamento");
+
+        String nome = tfNomeEquipamento.getText();
+        Esporte esporte = esporteInput.getValue();
+        Integer quantidade = Integer.valueOf(tfQuantidade.getText());
+        Estado estado = estadoInput.getValue();
+
+        equipamentoRepository.cadastrar(nome, esporte, quantidade, estado);
+
     }
 
     @FXML
     public void estadoSelecionado() {
-        System.out.println("Estado Selecionado");
+        System.out.println(estadoInput.getValue());
     }
 
     @FXML
     public void esporteSelecionado() {
         System.out.println("Esporte Selecionado");
+    }
+
+    @Override
+    public void initialize(URL arg0, ResourceBundle arg1) {
+        esporteDao = new EsporteJDBC(fabricaConexoes);
+        estadoInput.getItems().clear();
+        esporteInput.getItems().clear();
+        estadoInput.getItems().addAll(Estado.values());
+        esporteInput.getItems().addAll(esporteDao.buscarTodos());
     }
 
 }
